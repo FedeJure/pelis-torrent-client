db368fc9f9067d743b0b20dab95c477684cfde68
Federico Jure <fjure@atixlabs.com>
2020-06-21 20:36:15 -0300

    mejoro sistema de scroll infinito

--- src/movieGrid/MovieGrid.jsx
+++ src/movieGrid/MovieGrid.jsx
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react'
 import VisibilitySensor from 'react-visibility-sensor'
+import InfiniteScroll from 'react-infinite-scroller'
 import { getTrendingMovies } from '../api'
 import { getMovieDto } from '../domain/movie'
 import './MovieGrid.css'
 import { loadOptions } from '@babel/core';
-var time = 0;
+
 const storageKey = "homeMovieList";
 
 const MovieGrid = ({selectMovie, active}) => {
@@ -13,72 +14,56 @@ const MovieGrid = ({selectMovie, active}) => {
     const [loadMore, setLoadMore] = useState(true);
 
     const getDto = movie => {
-        return { ...getMovieDto(movie), delay: time++}
+        return getMovieDto(movie);
+    }
+
+    const saveNewMoviesInStorage = newMovies => {
+        const oldMovies = JSON.parse(localStorage.getItem(storageKey)) || [];
+        localStorage.setItem(storageKey, JSON.stringify([...oldMovies, ...newMovies]));
+    }
+
+    const dtoListToElementList = dtoList => {
+        return dtoList.map(dto => (
+            <MovieElement movie={dto} onClick={_ => selectMovie(dto)}/>
+        ));
     }
 
     const fetchMoviePage = async () => {
-        console.log("fetch more movies")
         const count = 50;
         const cachedList = JSON.parse(localStorage.getItem(storageKey)) || [];
         if (cachedList.length > count * actualPage) {
-            const aux = [...movies, ...cachedList.slice(count*(actualPage-1), count*actualPage)];
+            const aux = [...movies, ...dtoListToElementList(cachedList.slice(count*(actualPage-1), count*actualPage))];
             setActualPage(actualPage + 1);
             setMovies(aux);
-            time = 0;
             return;
         }
         getTrendingMovies(count, actualPage, result => {
-            const aux = [...movies, ...result.data.movies.map(getDto)];
-            time = 0;
-            localStorage.setItem(storageKey, JSON.stringify(aux));
+            const newMoviesDto = result.data.movies.map(getDto);
+            const aux = [...movies, ...dtoListToElementList(newMoviesDto)];
+            saveNewMoviesInStorage(newMoviesDto);
             setMovies(aux);    
             setActualPage(actualPage + 1);
         });
     }
 
-    useEffect(() => {
-        if (loadMore) fetchMoviePage();
-    }, [loadMore]);
-
-
-    const onVisivilityChange = async visible => {
-        console.log(visible)
-        setLoadMore(visible);
-    }
-
     return (
-        active && <VisibilitySensor onChange={onVisivilityChange} partialVisibility="bottom">
+        active && <InfiniteScroll
+        pageStart={0}
+        loadMore={fetchMoviePage}
+        hasMore={true}
+        loader={<div className="loader" key={0}>Loading ...</div>}>
             <div className="movieGrid">
-                {movies.map((movie) => (
-                    <MovieElement movie={movie} onClick={_ => selectMovie(movie)} timeToInit={movie.delay * 10}/>
-                ))}
+                {movies}
             </div>
-        </VisibilitySensor>
+        </InfiniteScroll>
     )
 };
 
-const MovieElement = ({movie, onClick, timeToInit}) => {
-    const [show, setShow] = useState(false);
-
-    useEffect(() => {
-        const effect = setTimeout(() => {
-            setShow(true);
-        },timeToInit);
-
-        const cleanUp = () => {
-            clearTimeout(effect);
-        }
-
-        return cleanUp;
-        
-    });
-
-    return (
-        show && <div className={`movieElement animate__animated animate__zoomIn`} key={movie.title} onClick={onClick}>
-            <img src={movie.image}/>
-            <p>{movie.title}</p>
-            <span>{movie.year}</span>
-        </div>
-    )
-}
+const MovieElement = ({movie, onClick}) => (
+    <div className={`movieElement animate__animated animate__zoomIn`} key={movie.title} onClick={onClick}>
+        <img src={movie.image}/>
+        <p>{movie.title}</p>
+        <span>{movie.year}</span>
+    </div>
+);
 export default MovieGrid;
