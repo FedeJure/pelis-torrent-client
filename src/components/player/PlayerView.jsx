457260f1f9155c7a9619a8e62d9cb682abc522de
Federico Jure <fjure@atixlabs.com>
2020-07-15 01:48:52 -0300

    agrego subtitulos

--- src/components/player/PlayerView.jsx
+++ src/components/player/PlayerView.jsx
@@ -1,17 +1,25 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, Children } from "react";
 import { getTorrentUrl } from '../../WebtorrentClient/WebtorrentClient';
 import ReactPlayer from 'react-player'
 
 import "./PlayerView.css";
-const PlayerView = ({ image, videoUrl }) => 
-      (<div className="playerView">
+const PlayerView = ({ image, videoUrl, availableSubtitles }) => 
+      (
+      <div className="playerView" crossOrigin="anonymous">
         <ReactPlayer url={videoUrl}
         className='react-player'
         controls
         width='100%'
         height='100%'
         light={image ? image : true}
-        />
+        config={{ file: {
+            attributes: {
+              crossOrigin: "anonymous"
+            },
+            tracks:
+             availableSubtitles && availableSubtitles.map(sub => ({kind: 'subtitles', src: sub.url, srcLang: sub.languageName, default: true, crossOrigin:"anonymous"}))
+          }}}>
+        </ReactPlayer>
     </div>
 );
 
