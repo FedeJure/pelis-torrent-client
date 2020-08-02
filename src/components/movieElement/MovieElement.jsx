0618cda2d7a39b766a11b68a40573e0ee91f469f
Federico Jure <fedejure@gmail.com>
2020-08-02 20:09:04 -0300

    improve movie element

--- src/components/movieElement/MovieElement.jsx
+++ src/components/movieElement/MovieElement.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './MovieElement.css'
 
-const MovieElement = ({movie, onClick}) => (
-    <div className="movieElement" key={movie.title} onClick={onClick}>
-        <img src={movie.image}/>
-        <p>{movie.title}</p>
-        <span>{movie.year}</span>
-    </div>
-);
+const MovieElement = ({movie, onClick}) => {
+    const [loaded, setLoaded] = useState(false);
+    return (
+        <><div className={`movieElement ${!loaded ? "emptyMovieElement" : ""}`} key={movie.title} onClick={onClick}>
+            <img src={movie.image} onLoad={() => setLoaded(true)} style={{display: loaded? "unset": "none"}}/>
+            {!loaded && <img src={process.env.PUBLIC_URL + "/imageLoading.gif"}/>}
+            <p>{movie.title}</p>
+            <span>{movie.year}</span>
+        </div>
+        {!loaded && <EmptyMovieElement/>}
+        </>
+    );
+};
 
 const EmptyMovieElement = () => (
     <div className="movieElement emptyMovieElement" key={Math.random()}>
