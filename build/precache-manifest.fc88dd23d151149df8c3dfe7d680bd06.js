a47dc427171d3e14c39c7c37561c113efc9f378e
Federico Jure <fjure@atixlabs.com>
2020-08-03 18:35:56 -0300

    agrego carpeta build

---
+++ build/precache-manifest.fc88dd23d151149df8c3dfe7d680bd06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09e52f22ffb1e1dbad1dc7a829e7ad73",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c9a9d91f79b1a7eea269",
+    "url": "/static/css/main.02190cdd.chunk.css"
+  },
+  {
+    "revision": "e577a002bf8a06bbef4a",
+    "url": "/static/js/2.47da7961.chunk.js"
+  },
+  {
+    "revision": "91cd73352bfaf272edc4116570d78879",
+    "url": "/static/js/2.47da7961.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9a9d91f79b1a7eea269",
+    "url": "/static/js/main.db4a8405.chunk.js"
+  },
+  {
+    "revision": "38ac821ea7d85669e9be",
+    "url": "/static/js/runtime-main.621f0842.js"
+  }
+]);
