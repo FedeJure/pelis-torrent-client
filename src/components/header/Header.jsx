e338b605e09841eb3531ea8d18e7d2596ae18151
Federico Jure <fedejure@gmail.com>
2021-02-05 21:36:24 -0300

    fix search bar issue

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -43,7 +43,7 @@ const Header = ({ isSerie, elements }) => {
         if (data.movies && data.movies.length > 0) {
             const movie = getMovieDto(data.movies[0]);
             MoviesRepository.saveMovie(movie);
-            history.push(Routes.getMovieUrl(movie.imdbCode, movie.title));
+            history.push(Routes.getMovieUrl(imdb_id, movie.title));
             window.location.reload();
         }
     }
